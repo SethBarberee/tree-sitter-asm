6f6b8dc2deebac1419914c9886a0045099607213
Seth Barberee <seth.barberee@gmail.com>
2022-09-06 19:01:51 -0500

    add GH actions and prettier format

--- grammar.js
+++ grammar.js
@@ -1,78 +1,46 @@
 module.exports = grammar({
-  name: 'asm',
+  name: "asm",
 
   rules: {
-    source_file: $ => choice(
+    source_file: ($) =>
+      choice(
         repeat($.function_definition),
         repeat($.directive),
-        repeat($.comment),
-    ),
+        repeat($.comment)
+      ),
 
-    function_definition: $ => seq(
-        $.function_label,
-        repeat($._statement),
-    ),
+    function_definition: ($) => seq($.function_label, repeat($._statement)),
 
+    _statement: ($) =>
+      choice(
+        $.return_statement,
+        $.branch_statement,
+        $.math_statement,
+        $.mov_statement
+      ),
 
-    _statement: $ => choice(
-      $.return_statement,
-      $.branch_statement,
-      $.math_statement,
-      $.mov_statement,
-    ),
+    math_statement: ($) =>
+      seq($.opcode, $.register, ",", $.register, ",", $.register),
 
-    math_statement: $ => seq(
-        $.opcode,
-        $.register,
-        ',',
-        $.register,
-        ',',
-        $.register,
-    ),
-
-    mov_statement: $ => seq(
-        $.opcode,
-        $.register,
-        ',',
-        choice(
-            $.register,
-            $.constant,
-        ),
-    ),
+    mov_statement: ($) =>
+      seq($.opcode, $.register, ",", choice($.register, $.constant)),
 
     // TODO: fairly limited and only to ARM/THUMB for now since that's all I use
-    opcode: $ => choice(
-        /sub+/,
-        /add+/,
-        /mul+/,
-        /mov+/,
-        /stm+/,
-        /str+/,
-    ),
+    opcode: ($) => choice(/sub+/, /add+/, /mul+/, /mov+/, /stm+/, /str+/),
 
     // TODO: look into making this better
-    function_label: $ => /[A-z]+[:]/,
+    function_label: ($) => /[A-z]+[:]/,
 
-    return_statement: $ => seq(
-            /(bx)\s+/,
-            $.register,
-    ),
+    return_statement: ($) => seq(/(bx)\s+/, $.register),
 
-    branch_statement: $ => /(bl)\s+[A-z]+/,
+    branch_statement: ($) => /(bl)\s+[A-z]+/,
 
-    register: $ => choice(
-            /[r]\d+/,
-            /sp/,
-            /lr/,
-            /pc/,
-    ),
+    register: ($) => choice(/[r]\d+/, /sp/, /lr/, /pc/),
 
-    directive: $ => /[.][a-z]+.*/,
+    directive: ($) => /[.][a-z]+.*/,
 
-    comment: $ => /@.*/,
+    comment: ($) => /@.*/,
 
-    constant: $ => /\d+/,
-
-  }
+    constant: ($) => /\d+/,
+  },
 });
-
